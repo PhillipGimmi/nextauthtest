166069b49554a13449550c08e0f14ad66718bfa2
Phillip-hyperboliq <phillip.gimmi@hyperboliq.com>
2025-01-25 17:52:46 +0200

    add auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
 // src/app/layout.tsx
-import Script from 'next/script'
-
 export default function RootLayout({
   children,
 }: {
@@ -9,21 +7,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script 
-          src="https://cdn.dashauth.com/sdk/v1/dashauth.js" 
-          strategy="beforeInteractive"
-        />
-        <Script
-          id="dash-auth-init"
-          strategy="afterInteractive"
-        >
-          {`
-            DashAuth.init({
-              clientId: 'a5fd4c90e50562b20972cd3f5f9762559888c51e842f49e250c12046e11761f4',
-              domain: 'authxero.com'
-            });
-          `}
-        </Script>
+
+<script 
+  src="https://cdn.authxero.com/sdk/v1/authxero.js"
+  data-client-id="authxero.com"
+  data-domain="authxero.com"
+  async
+></script>
       </head>
       <body>
         {children}
