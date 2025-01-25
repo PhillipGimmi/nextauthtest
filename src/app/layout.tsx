47206b8d4c6dcceb72f2b07550e8afc43ca9f371
Phillip-hyperboliq <phillip.gimmi@hyperboliq.com>
2025-01-25 23:43:03 +0200

    chore

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,11 +7,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <script 
-  src="https://authxero.com"
-  data-client-id="a5fd4c90e50562b20972cd3f5f9762559888c51e842f49e250c12046e11761f4"
-  defer
-></script>
+      <script
+        src="https://authxero.com"
+        data-client-id="a5fd4c90e50562b20972cd3f5f9762559888c51e842f49e250c12046e11761f4"
+        data-environment="development"
+        data-version="v1"
+        defer
+        data-platform="traditional"
+      ></script>
       </head>
       <body>
         {children}
