100b3a47e2b3fd09adb1e4b953b6c674af5f180a
Phillip-hyperboliq <phillip.gimmi@hyperboliq.com>
2025-03-10 21:45:51 +0200

    chore

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <script src="https://dashauth.com/secure.js" defer id="2c436c5b-fa7a-4914-8c5a-0d7e52b27fac"></script>
+      <script src="https://www.dashauth.com/secure.js" defer id="f013313b-d38c-4d6c-8677-b0262d27ca65"></script>
       </head>
       <body>
         {children}
